446ce397e700f288ebd1f17cb549a044346c2af6
neighborpil <neighborpil@naver.com>
2020-06-27 01:06:40 +0900

    widget tests

--- ch04_basic_widgets/lib/main.dart
+++ ch04_basic_widgets/lib/main.dart
@@ -26,32 +26,95 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: 'My home'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 40.0,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
-              letterSpacing: 4.0 // 자간
-            ),
+          Checkbox(
+            value: isChecked,
+            onChanged: (value) => {
+              setState(() => {
+                isChecked = value,
+                print('isChecked: $isChecked')
+              })
+            },
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Switch(
+            value: isChecked,
+              onChanged: (value) => {
+                setState(() {
+                  isChecked = value;
+                })
+              },
           )
         ],
       )
     );
   }
 }
+
+//
+//class MyHomePage extends StatelessWidget {
+//  var isChecked = false;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      body: Column(
+//        mainAxisSize: MainAxisSize.max,
+//        mainAxisAlignment: MainAxisAlignment.start,
+//        crossAxisAlignment: CrossAxisAlignment.center,
+//        children: <Widget>[
+//          TextField(),
+//          TextField(
+//            decoration: InputDecoration(
+//              labelText: '여기에 입력하세요' // 힌트
+//            ),
+//          ),
+//          TextField(
+//            decoration: InputDecoration(
+//              border: OutlineInputBorder(),
+//              labelText: '여기 입력해'
+//            ),
+//          ),
+//          Checkbox(
+//            value: isChecked,
+//            onChanged: (value)  => {
+//              setState(() {
+//                isChecked = value;
+//              })
+//            }
+//          )
+//        ],
+//      )
+//    );
+//  }
+//}
